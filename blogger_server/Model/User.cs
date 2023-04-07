13a4a5ce5d843339938cfa2e83d281cadbbc3fc3
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2023-04-08 00:23:35 +1200

    finished the model class for the users

---
+++ blogger_server/Model/User.cs
@@ -0,0 +1,15 @@
+﻿namespace blogger_server.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserFirstName { get; set; }
+        public string UserLastName { get; set; }
+        public string UserEmail { get; set; }
+        public string UserMobileNumber { get; set; }
+        public string UserGender { get; set; }
+
+        public string UserUserName { get; set; }
+        public string UserPassword { get; set; }
+    }
+}
