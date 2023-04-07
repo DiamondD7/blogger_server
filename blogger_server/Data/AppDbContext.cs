1b805fd56b0f7b232d664984b6cf7cddcfcf61dd
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2023-04-08 00:03:51 +1200

    added DbContext file in the data folder. Partially Completed. Needed Model for tables

---
+++ blogger_server/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace blogger_server.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+
+        }
+
+    }
+}
