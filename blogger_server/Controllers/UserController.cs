7a339244cb9f48d7b24dbf091a51db6e6ae64925
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2023-04-09 00:05:04 +1200

    added the HtppGet for all the users

---
+++ blogger_server/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using blogger_server.Data;
+using blogger_server.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace blogger_server.Controllers
+{
+    public class UserController
+    {
+        private readonly AppDbContext _context;
+        public UserController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUser()
+        {
+            var users = await _context.users.ToListAsync();
+            return users;
+        }
+    }
+}
--- blogger_server/Data/AppDbContext.cs
+++ blogger_server/Data/AppDbContext.cs
@@ -1,13 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using blogger_server.Model;
+using Microsoft.EntityFrameworkCore;
 
 namespace blogger_server.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
 
         }
 
+        public DbSet<User> users { get; set; }
     }
 }
