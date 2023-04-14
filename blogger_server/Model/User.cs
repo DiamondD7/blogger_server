5cf03ede1d762f72bf4984cc62b7b77b749f71d7
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2023-04-14 12:57:23 +1200

    Added data annotation and validation in the model

--- blogger_server/Model/User.cs
+++ blogger_server/Model/User.cs
@@ -1,15 +1,23 @@
-﻿namespace blogger_server.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace blogger_server.Model
 {
     public class User
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string UserFirstName { get; set; }
         public string UserLastName { get; set; }
+        [Required]
         public string UserEmail { get; set; }
+        [Required]
         public string UserMobileNumber { get; set; }
         public string UserGender { get; set; }
 
+        [Required(ErrorMessage = "Username is required")]
         public string UserUserName { get; set; }
+        [Required(ErrorMessage = "Password is required")]
         public string UserPassword { get; set; }
     }
 }
